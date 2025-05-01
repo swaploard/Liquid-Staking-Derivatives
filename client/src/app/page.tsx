de8ee4a396962cd1b0d15c204fd5dddedc4c648a
swaploard <nikhil876706@gmail.com>
2025-05-01 15:02:55 +0530

    components

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
 import DashboardPage from "@/components/dashboard-page"
-
 export default function Home() {
   return (
-  <div>
-    <DashboardPage />
-  </div>
+    <div>
+      <DashboardPage />
+    </div>
   );
 }
