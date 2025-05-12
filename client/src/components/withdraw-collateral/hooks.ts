c91d595869b553545354cf2410996e001f0a8c1c
LORD-Nikhil-Patil <Nikhil876706@gmail.com>
2025-05-12 12:47:11 +0530

    feat(withdraw-collateral): add custom hook for handling collateral withdrawal
    Implement a custom hook `useWithdrawCollateral` to manage the withdrawal process, including transaction handling, step updates, and loading states. This hook integrates with the `wagmi` library and the CollateralVault contract to ensure a smooth user experience during the withdrawal process.

---
+++ client/src/components/withdraw-collateral/hooks.ts
@@ -0,0 +1,63 @@
+import { useWriteContract, useAccount, useReadContract, usePublicClient } from "wagmi"
+import CollateralVault from "@/contracts/CollateralVault.json";
+import { Address } from "viem";
+import { Step, StepStatus } from "@/types";
+import { useEffect, useState } from "react";
+
+const vaultContract = process.env.NEXT_PUBLIC_VAULT_CONTRACT_ADDRESS
+
+interface useWithdrawCollateralProps {
+    setSteps: React.Dispatch<React.SetStateAction<Step[]>>
+    setShowStepper: React.Dispatch<React.SetStateAction<boolean>>
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+export const useWithdrawCollateral = ({ setSteps, setShowStepper, setIsLoading }: useWithdrawCollateralProps) => {
+    const publicClient = usePublicClient()
+    const { writeContract } = useWriteContract()   
+    const { chainId, address: userAddress } = useAccount();
+
+    const updateStepStatus = (stepIndex: number, newStatus: StepStatus) => {
+        setSteps((prev) =>
+            prev.map((step, index) =>
+                index === stepIndex ? { ...step, status: newStatus } : step
+            )
+        );
+    };
+    const handleWithdrawCollateral = async (amount: bigint, token: string) => {
+       updateStepStatus(0, "current")
+        setShowStepper(true)
+        setIsLoading(true)
+        writeContract({
+            address: vaultContract as Address,
+            abi: CollateralVault.abi,
+            functionName: "withdrawCollateral",
+            args: [token, amount],
+            chainId: chainId
+        },{
+            onSuccess: async (txHash) => {
+                updateStepStatus(0, "completed")
+                updateStepStatus(1, "current")
+                const receipt = await publicClient?.waitForTransactionReceipt({
+                    hash: txHash,
+                });
+                if (receipt?.status.toLowerCase() === 'success') {
+                    updateStepStatus(1, "completed")
+                    setShowStepper(false)
+                    setIsLoading(false)
+                } else {
+                    setShowStepper(false)
+                    setIsLoading(false)
+                }
+            },
+            onError: (err) => {
+                setShowStepper(false)
+                setIsLoading(false)
+                console.log(err)
+            }
+        }) 
+    }
+
+    return{
+        handleWithdrawCollateral
+    }
+}
